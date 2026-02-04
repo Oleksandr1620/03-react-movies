ed1154892e3afea2023a11c3f3ddaf17cfe86ec3
Oleksandr <sov1620@gmail.com>
2026-02-04 23:53:23 +0200

    my third react-hw

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
-import type { Movie, MoviesResponse } from "../types/movie";
+import type { Movie } from "../types/movie";
 
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
+
+interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
 
 export const fetchMovies = async (
   query: string,
